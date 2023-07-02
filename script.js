2b25ed25c0d330b4e5d6a74f063b3d6a9daa6ef5
Determinated738 <118392487+Determinated738@users.noreply.github.com>
2023-07-02 14:33:38 -0400

    Fix for "mode"

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
+
 function main() {
   const inputData = fs.readFileSync('input.txt', 'utf8');
-  const regex = /\\\"button_name\\\":\\\"([^,]*?)\\\",(\\\"data\\\":\[.*?\]),\\\"mode\\\".*?,\\\"text\\\":\\\"(.*?)\\\"/g;
-  const outputData = inputData.replace(regex, (_match, buttonName, data, _text) => {
+  const regex = /\\\"button_name\\\":\\\"([^,]*?)\\\",(\\\"data\\\":\[.*?\]),\\\"mode\\\"[ ]*:[ ]*(.*?),\\\"text\\\":\\\"(.*?)\\\"/g;
+  const outputData = inputData.replace(regex, (_match, buttonName, data, mode, _text) => {
     const commands = data.match(/\\\"cmd_line\\\":\\\"(.*?)\\\",(\\\"cmd_ver\\\":|$)/g);
     const commandLines = commands.map(cmd => {
       const cmdLine = cmd.match(/\\\"cmd_line\\\":\\\"(.*?)\\\",/);
       return cmdLine ? cmdLine[1].replace(/\\\\"/g, '\\\\\"') : '';
     });
     const joinedCommands = commandLines.join('\\\\n');
-    return `\\\"button_name\\\":\\\"${buttonName}\\\",${data},\\\"mode\\\".*?,\\\"text\\\":\\\"${joinedCommands}\\\"`;
+    return `\\\"button_name\\\":\\\"${buttonName}\\\",${data},\\\"mode\\\":${mode},\\\"text\\\":\\\"${joinedCommands}\\\"`;
   });
-
-  const fixedOutputData = outputData.replace(/(.*?)\.\*\?/g, '$1:0');
-  fs.writeFileSync('output.txt', fixedOutputData, 'utf8');
+  fs.writeFileSync('output.txt', outputData, 'utf8');
+  console.info("The NBT has been written to output.txt.")
 }
+
 main();
