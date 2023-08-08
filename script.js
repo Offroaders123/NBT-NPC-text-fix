f6276697210aa0d27624550af54addf783c2fa2f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-08-08 03:59:14 -0700

    SNBT Diff
    Added a command to run the script with `node --watch`, which is essentially live reload, but just for Node scripts. Super helpful to have for debugging the output of your scripts!
    
    Also added a script to get a diff of the prettified SNBT, as to help figure out what the RegEx calls are doing to the data. I like reverse engineering haha.

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ import * as fs from 'node:fs/promises';
 import * as nbt from 'nbtify';
 
 const inputData = await fs.readFile('input.txt', 'utf8');
-console.log(inputData,"\n");
+const inputFormatted = nbt.stringify(nbt.parse(inputData),{ space: 2 });
+console.log(nbt.parse(inputFormatted),"\n");
 
 const regex = /\\\"button_name\\\":\\\"([^,]*?)\\\",(\\\"data\\\":\[.*?\]),\\\"mode\\\"[ ]*:[ ]*(.*?),\\\"text\\\":\\\"(.*?)\\\"/g;
 
@@ -15,8 +16,12 @@ const outputData = inputData.replace(regex, (_match, buttonName, data, mode, _te
   const joinedCommands = commandLines.join('\\\\n');
   return `\\\"button_name\\\":\\\"${buttonName}\\\",${data},\\\"mode\\\":${mode},\\\"text\\\":\\\"${joinedCommands}\\\"`;
 });
-console.log(outputData,"\n");
+
+const outputFormatted = nbt.stringify(nbt.parse(outputData),{ space: 2 });
+console.log(nbt.parse(outputFormatted),"\n");
 
 await fs.writeFile('output.txt', outputData, 'utf8');
+await fs.writeFile('test/inputFormatted.txt', inputFormatted, 'utf8');
+await fs.writeFile('test/outputFormatted.txt', outputFormatted, 'utf8');
 
 console.info("The NBT has been written to output.txt.");
