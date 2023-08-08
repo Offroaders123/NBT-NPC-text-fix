3443a4e9b5173efc19982d74fc72268f3afeaa4b
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-08-08 03:32:37 -0700

    Script Flattening

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 import * as fs from 'node:fs/promises';
 import * as nbt from 'nbtify';
 
-async function main() {
-  const inputData = await fs.readFile('input.txt', 'utf8');
-  const inputFormatted = nbt.stringify(nbt.parse(inputData),{ space: 2 });
-  console.log(JSON.parse(nbt.parse(inputFormatted).tag.movingEntity.Occupants.map(occupant => occupant.SaveData.Actions).filter(Boolean)[0]),"\n");
+const inputData = await fs.readFile('input.txt', 'utf8');
+const inputFormatted = nbt.stringify(nbt.parse(inputData),{ space: 2 });
+console.log(JSON.parse(nbt.parse(inputFormatted).tag.movingEntity.Occupants.map(occupant => occupant.SaveData.Actions).filter(Boolean)[0]),"\n");
 
-  const regex = /\\\"button_name\\\":\\\"([^,]*?)\\\",(\\\"data\\\":\[.*?\]),\\\"mode\\\"[ ]*:[ ]*(.*?),\\\"text\\\":\\\"(.*?)\\\"/g;
+const regex = /\\\"button_name\\\":\\\"([^,]*?)\\\",(\\\"data\\\":\[.*?\]),\\\"mode\\\"[ ]*:[ ]*(.*?),\\\"text\\\":\\\"(.*?)\\\"/g;
 
-  const outputData = inputData.replace(regex, (_match, buttonName, data, mode, _text) => {
-    const commands = data.match(/\\\"cmd_line\\\":\\\"(.*?)\\\",(\\\"cmd_ver\\\":|$)/g);
-    const commandLines = commands.map(cmd => {
-      const cmdLine = cmd.match(/\\\"cmd_line\\\":\\\"(.*?)\\\",/);
-      return cmdLine ? cmdLine[1].replace(/\\\\"/g, '\\\\\"') : '';
-    });
-    const joinedCommands = commandLines.join('\\\\n');
-    return `\\\"button_name\\\":\\\"${buttonName}\\\",${data},\\\"mode\\\":${mode},\\\"text\\\":\\\"${joinedCommands}\\\"`;
+const outputData = inputData.replace(regex, (_match, buttonName, data, mode, _text) => {
+  const commands = data.match(/\\\"cmd_line\\\":\\\"(.*?)\\\",(\\\"cmd_ver\\\":|$)/g);
+  const commandLines = commands.map(cmd => {
+    const cmdLine = cmd.match(/\\\"cmd_line\\\":\\\"(.*?)\\\",/);
+    return cmdLine ? cmdLine[1].replace(/\\\\"/g, '\\\\\"') : '';
   });
-  const outputFormatted = nbt.stringify(nbt.parse(outputData),{ space: 2 });
-  console.log(JSON.parse(nbt.parse(outputFormatted).tag.movingEntity.Occupants.map(occupant => occupant.SaveData.Actions).filter(Boolean)[0]),"\n");
+  const joinedCommands = commandLines.join('\\\\n');
+  return `\\\"button_name\\\":\\\"${buttonName}\\\",${data},\\\"mode\\\":${mode},\\\"text\\\":\\\"${joinedCommands}\\\"`;
+});
+const outputFormatted = nbt.stringify(nbt.parse(outputData),{ space: 2 });
+console.log(JSON.parse(nbt.parse(outputFormatted).tag.movingEntity.Occupants.map(occupant => occupant.SaveData.Actions).filter(Boolean)[0]),"\n");
 
-  await fs.writeFile('output.txt', outputData, 'utf8');
-  await fs.writeFile('inputFormatted.txt', inputFormatted, 'utf8');
-  await fs.writeFile('outputFormatted.txt', outputFormatted, 'utf8');
+await fs.writeFile('output.txt', outputData, 'utf8');
+await fs.writeFile('inputFormatted.txt', inputFormatted, 'utf8');
+await fs.writeFile('outputFormatted.txt', outputFormatted, 'utf8');
 
-  console.info("The NBT has been written to output.txt.")
-}
-
-main();
+console.info("The NBT has been written to output.txt.");
