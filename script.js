7687c938dcdf7a3b467be1921bcc4d135bf11f58
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-08-08 03:31:10 -0700

    Formatting + FS Promises

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-import * as fs from 'node:fs';
+import * as fs from 'node:fs/promises';
 import * as nbt from 'nbtify';
 
-function main() {
-  const inputData = fs.readFileSync('input.txt', 'utf8');
+async function main() {
+  const inputData = await fs.readFile('input.txt', 'utf8');
   const inputFormatted = nbt.stringify(nbt.parse(inputData),{ space: 2 });
   console.log(JSON.parse(nbt.parse(inputFormatted).tag.movingEntity.Occupants.map(occupant => occupant.SaveData.Actions).filter(Boolean)[0]),"\n");
+
   const regex = /\\\"button_name\\\":\\\"([^,]*?)\\\",(\\\"data\\\":\[.*?\]),\\\"mode\\\"[ ]*:[ ]*(.*?),\\\"text\\\":\\\"(.*?)\\\"/g;
+
   const outputData = inputData.replace(regex, (_match, buttonName, data, mode, _text) => {
     const commands = data.match(/\\\"cmd_line\\\":\\\"(.*?)\\\",(\\\"cmd_ver\\\":|$)/g);
     const commandLines = commands.map(cmd => {
@@ -17,9 +19,11 @@ function main() {
   });
   const outputFormatted = nbt.stringify(nbt.parse(outputData),{ space: 2 });
   console.log(JSON.parse(nbt.parse(outputFormatted).tag.movingEntity.Occupants.map(occupant => occupant.SaveData.Actions).filter(Boolean)[0]),"\n");
-  fs.writeFileSync('output.txt', outputData, 'utf8');
-  fs.writeFileSync('inputFormatted.txt', inputFormatted, 'utf8');
-  fs.writeFileSync('outputFormatted.txt', outputFormatted, 'utf8');
+
+  await fs.writeFile('output.txt', outputData, 'utf8');
+  await fs.writeFile('inputFormatted.txt', inputFormatted, 'utf8');
+  await fs.writeFile('outputFormatted.txt', outputFormatted, 'utf8');
+
   console.info("The NBT has been written to output.txt.")
 }
 
