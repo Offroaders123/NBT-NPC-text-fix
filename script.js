aa78d27e5ea98e5f016286060d4b88eb1b4b3d41
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-08-08 03:13:48 -0700

    Hacky Actions
    Very hackily extended the script so it can read the `SaveData.Actions` values as JSON, meaning they are full-on JS objects you can manipulate!

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 
-function main() {
+async function main() {
+  const nbt = await import("nbtify");
   const inputData = fs.readFileSync('input.txt', 'utf8');
+  const inputFormatted = nbt.stringify(nbt.parse(inputData),{ space: 2 });
+  console.log(JSON.parse(nbt.parse(inputFormatted).tag.movingEntity.Occupants.map(occupant => occupant.SaveData.Actions).filter(Boolean)[0]),"\n");
   const regex = /\\\"button_name\\\":\\\"([^,]*?)\\\",(\\\"data\\\":\[.*?\]),\\\"mode\\\"[ ]*:[ ]*(.*?),\\\"text\\\":\\\"(.*?)\\\"/g;
   const outputData = inputData.replace(regex, (_match, buttonName, data, mode, _text) => {
     const commands = data.match(/\\\"cmd_line\\\":\\\"(.*?)\\\",(\\\"cmd_ver\\\":|$)/g);
@@ -12,7 +15,11 @@ function main() {
     const joinedCommands = commandLines.join('\\\\n');
     return `\\\"button_name\\\":\\\"${buttonName}\\\",${data},\\\"mode\\\":${mode},\\\"text\\\":\\\"${joinedCommands}\\\"`;
   });
+  const outputFormatted = nbt.stringify(nbt.parse(outputData),{ space: 2 });
+  console.log(JSON.parse(nbt.parse(outputFormatted).tag.movingEntity.Occupants.map(occupant => occupant.SaveData.Actions).filter(Boolean)[0]),"\n");
   fs.writeFileSync('output.txt', outputData, 'utf8');
+  fs.writeFileSync('inputFormatted.txt', inputFormatted, 'utf8');
+  fs.writeFileSync('outputFormatted.txt', outputFormatted, 'utf8');
   console.info("The NBT has been written to output.txt.")
 }
 
